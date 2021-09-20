cca915f11a4bd1e26e29d1d2ef4c99ea3acc15ed
Thum Chee Chon <bladegun@gmail.com>
2021-09-20 21:17:15 +0800

    Install @ionic-native/wikitude package from github using napa.

--- scripts/install-wikitude.js
+++
@@ -1,8 +0,0 @@
-const { resolve } = require('path');
-const { emptyDirSync, copySync } = require('fs-extra');
-
-const src = resolve(__dirname, '..', '..', 'ionic-native', 'dist', '@ionic-native', 'plugins', 'wikitude');
-const dest = resolve(__dirname, '..', 'node_modules', '@ionic-native', 'wikitude');
-
-emptyDirSync(dest);
-copySync(src, dest);
